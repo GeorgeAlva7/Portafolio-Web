676fd9ca766b316038b1630ca6f494521bd957c4
GeorgeAlva7 <98796254+GeorgeAlva7@users.noreply.github.com>
2022-01-31 23:56:09 -0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,114 @@
+window.sr = ScrollReveal();
+
+    sr.reveal('.navbar', {
+        duration: 800,
+        origin: 'bottom',
+        distance: '-300px'
+    });
+
+    sr.reveal('.container-fluid', {
+        duration: 800,
+        origin: 'bottom',
+        distance: '-400px'
+    });
+    
+
+    sr.reveal('.nombre .h1',{
+        duration: 1200,
+        origin: 'bottom',
+        distance: '-300px'
+    });
+    
+    sr.reveal('.nombre .h5',{
+        delay: 1000,
+        duration: 1000,
+        origin: 'bottom',
+        distance: '-70px'
+    });
+
+    sr.reveal('.contenedor', {
+        duration: 800,
+        delay: 200
+    });
+
+    sr.reveal('.sobremi', {
+        duration: 1500,
+        delay: 200
+    });
+
+    sr.reveal('.logros', {
+        duration: 1500,
+        delay: 200
+    });
+
+    sr.reveal('.proyectos', {
+        duration: 1500,
+        delay: 200
+    });
+
+    sr.reveal('.contacto', {
+        duration: 1500,
+        delay: 200
+    });
+
+    // SOBREMI.HMTL y  LOGROS.HTML 
+
+    sr.reveal('.titulo',{
+        delay: 500,
+        duration: 500,
+        origin: 'left',
+        distance: '160px'
+    });
+
+    sr.reveal('div .contenedor .center', {
+        delay: 700,
+        duration: 1500,
+        origin: 'left',
+        distance: '160px'
+    });
+
+    // PROYECTOS.HTML
+
+    sr.reveal('.tit .titulo',{
+        delay: 0,
+        duration: 1200,
+        origin: 'left',
+        distance: '200px'
+    });
+    
+    sr.reveal('.galeria1 div center',{
+        delay: 200,
+        duration: 1200,
+        origin: 'left',
+        distance: '200px'
+    })
+    
+    sr.reveal('.galeria1 .carousel',{
+        delay: 500,
+        duration: 1200,
+        origin: 'left',
+        distance: '200px'
+    })
+
+    sr.reveal('.galeria2 div center',{
+        duration: 500,
+        origin: 'left',
+        distance: '200px'
+    })
+    
+    sr.reveal('.galeria2 .carousel',{
+        delay: 200,
+        duration: 1200,
+        origin: 'left',
+        distance: '200px'
+    })
+    
+
+  /*  
+    sr.reveal('',{
+        duration: ,
+        origin: ,
+        distance: 
+    });
+
+    */
